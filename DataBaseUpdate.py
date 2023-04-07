b95860518ee7e88d18a9a95efb260420dde1b6e3
saadaoui <saadaoui.wassim.96@gmail.com>
2023-04-07 16:00:34 +0100

    tek-up

---
+++ DataBaseUpdate.py
@@ -0,0 +1,36 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred,{
+    'databaseURL':"https://facerealtime-787b9-default-rtdb.firebaseio.com/"
+})
+
+ref = db.reference('Students')
+data = {
+    "152183":
+        {
+            "name": "Wassim",
+            "field": "Cyber Security",
+            "group": "SSIR-C",
+            "year": 2,
+            "total_attandance": 7,
+            "last_attendance": "2022-12-11 14:51:00"
+        },
+"349701":
+        {
+            "name": "Othmen",
+            "field": "software enginer",
+            "group": "GLS-B",
+            "year": 2,
+            "total_attandance": 1,
+            "last_attendance": "2023-01-27 11:25:00"
+        }
+}
+
+
+for key, value in data.items():
+    ref.child(key).set(value)
